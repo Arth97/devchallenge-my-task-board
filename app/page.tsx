5d4f600540f2c1fcbb598c9d86e0752970c27f91
Arturo <arturotinajas@gmail.com>
2025-08-10 04:02:11 +0200

    Changes

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 	return (
 		<main className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
 			<div className="max-w-[555px]">
-				<h1 className="title">(emoji) My Task Board (lapiz)</h1>
+				<h1 className="title">(Logo) My Task Board (lapiz)</h1>
 				<p className="description">Tasks to keep organised</p>
 				<div className="flex flex-col justify-center gap-5 mt-10">
 					<Task status={"todo"} title={title} />
--- components/Task.tsx
+++ components/Task.tsx
@@ -20,11 +20,29 @@ const Task = ({status, title}:TaskProps) => {
     todo: "bg-gray-light",
   }
 
+  const taskEmoji = {
+    progress: "⏰",
+    completed: "🏋️",
+    wontdo: "☕️",
+    todo: "📚",
+  }
+  
+  const statusEmoji = {
+    progress: "",
+    completed: "",
+    wontdo: "",
+    todo: "",
+  }
+
 	return (
-		<div className={`w-[555px] h-[75px] flex flex-row items-center p-4 rounded-2xl ${taskBgStyle[status]}`}>      
-      <div className="size-11 rounded-xl bg-background"></div>
+		<div className={`w-[555px] h-[75px] flex flex-row items-center gap-4 p-4 rounded-2xl ${taskBgStyle[status]}`}>      
+      <div className="size-11 rounded-xl flex items-center justify-center bg-background">
+        {taskEmoji[status]}
+      </div>
       <h2 className="task-title">{title}</h2>
-      <div className={`size-11 rounded-xl ml-auto ${taskBgStatus[status]}`}></div>
+      <div className={`size-11 rounded-xl ml-auto flex items-center justify-center ${taskBgStatus[status]}`}>
+        {statusEmoji[status]}
+      </div>
     </div>
 	);
 }
